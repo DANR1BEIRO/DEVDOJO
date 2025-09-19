3b3907cd542c19d70443aee6d535daae04bec9f1
DANR1BEIRO <danribeirodev@gmail.com>
2025-09-19 17:55:37 -0300

    Aula:  159 - Classes Utilitárias - Serialization pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/X_Serializable/test/SerializacaoTest01.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.X_Serializable.test;
+
+import academy.devdojo.maratonajava.javacore.X_Serializable.domain.Aluno;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SerializacaoTest01 {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno(1L, "Goku", "sayajin");
+        serializar(aluno);
+
+    }
+
+    private static void serializar(Aluno aluno) {
+        Path path = Paths.get("pasta/aluno.ser");
+        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(path))) {
+            oos.writeObject(aluno);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
