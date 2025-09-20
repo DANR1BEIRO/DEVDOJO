395637ada04afa3aeaf80d12a7ab5388c6c46918
DANR1BEIRO <danribeirodev@gmail.com>
2025-09-20 09:44:40 -0300

    Aulas: 163/164 - Coleções pt 03 e 04 - hashCode

--- src/academy/devdojo/maratonajava/javacore/Y_Collections/domain/Android.java
+++ src/academy/devdojo/maratonajava/javacore/Y_Collections/domain/Android.java
@@ -9,6 +9,15 @@ public class Android {
         this.name = name;
     }
 
+    /**
+     * Características intrínsecas ao equals:
+     * Reflexivo: x.equals(y) precisa retornar true para tudo que for diferente de null.
+     * Simetria: Para x e y diferentes de null, se x.equals(y) == true, logo, y.equals(x) == true.
+     * Transitividade: Para x, y, e z diferentes de null, se x.equal(y) == true e x.equals(z) == true,
+     * logo y.equals(z) == true.
+     * Consistência: x.equals(x) sempre retorna true se x for diferente de null.
+     * Para x diferente de null, x.equals(null) sempre retornará false.
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) return false;
@@ -16,6 +25,18 @@ public class Android {
         if (this.getClass() != obj.getClass()) return false;
         Android android = (Android) obj;
         return serial != null && serial.equals(android.serial);
+    }
+
+    /**
+     * Características intrínsecas ao hashCode:
+     * Se x.equals(y) == true, y.hashCode() == x.hashCode().
+     * y.hashCode() == x.hashCode() não significa que y.equals(x) precisa ser true.
+     * Se y.hashCode() != x.hashCode(), então x.equal(y) sempre será false.
+     *
+     */
+    @Override
+    public int hashCode() {
+        return this.serial == null ? 0 : this.serial.hashCode();
     }
 
     public String getSerial() {
