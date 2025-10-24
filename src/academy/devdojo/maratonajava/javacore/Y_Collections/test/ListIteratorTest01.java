ef0fda133767966eeb0baf09cd00bb5752a276e9
DANR1BEIRO <danribeirodev@gmail.com>
2025-10-24 11:04:00 -0300

    test: Adiciona testes para manipulacão segura de listas com ListIterator

---
+++ src/academy/devdojo/maratonajava/javacore/Y_Collections/test/ListIteratorTest01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Y_Collections.test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+public class ListIteratorTest01 {
+    static void main() {
+        ArrayList<String> nomes = new ArrayList<>();
+        nomes.add("Goku");
+        nomes.add("vegeta");
+        nomes.add("gohan");
+
+        ListIterator<String> nomesIterador = nomes.listIterator();
+        System.out.println(nomes);
+        while (nomesIterador.hasNext()) {
+            nomesIterador.next();
+        }
+        nomesIterador.add("Bulma");
+        System.out.println(nomes);
+    }
+}
