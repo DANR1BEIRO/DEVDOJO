2bf250e7603e05599301c426588650bebe180f09
DANR1BEIRO <danribeirodev@gmail.com>
2025-12-26 11:01:25 -0300

    Aula 215: Streams pt 13 - Collectors pt 03 - Grouping by pt 02

---
+++ src/academy/devdojo/maratonajava/javacore/ZZE_Streams/domain/Promotion.java
@@ -0,0 +1,6 @@
+package academy.devdojo.maratonajava.javacore.ZZE_Streams.domain;
+
+public enum Promotion {
+    UNDER_PROMOTION,
+    NORMAL_PRICE
+}
--- src/academy/devdojo/maratonajava/javacore/ZZE_Streams/test/StreamTest012_groupingByInsideAnotherGroupingBy.java
+++ src/academy/devdojo/maratonajava/javacore/ZZE_Streams/test/StreamTest012_groupingByInsideAnotherGroupingBy.java
@@ -2,13 +2,14 @@ package academy.devdojo.maratonajava.javacore.ZZE_Streams.test;
 
 import academy.devdojo.maratonajava.javacore.ZZE_Streams.domain.Category;
 import academy.devdojo.maratonajava.javacore.ZZE_Streams.domain.LightNovel;
+import academy.devdojo.maratonajava.javacore.ZZE_Streams.domain.Promotion;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class StreamTest011_groupingBy {
+public class StreamTest012_groupingByInsideAnotherGroupingBy {
     private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
             new LightNovel("Lone Wolf & Cub", 8.99, Category.SEINEN),
             new LightNovel("Evangelion", 3.99, Category.SEINEN),
@@ -21,11 +22,12 @@ public class StreamTest011_groupingBy {
             new LightNovel("Dragon Ball", 4.00, Category.SHOUNEN)));
 
     static void main() {
+        Map<Promotion, List<LightNovel>> collect = lightNovels.stream()
+                .collect(Collectors.groupingBy(lightNovel -> lightNovel.getPrice() < 5 ? Promotion.UNDER_PROMOTION : Promotion.NORMAL_PRICE));
 
-        Map<Category, List<LightNovel>> collect = lightNovels.stream()
-                .collect(Collectors.groupingBy(LightNovel::getCategory));
+        collect.forEach(((promotion, lightNovels1) ->
+                System.out.println(promotion + ": " + lightNovels1)));
 
-        collect.forEach(((category, lightNovels1) ->
-                System.out.println(category + ": " + lightNovels1)));
+
     }
 }
