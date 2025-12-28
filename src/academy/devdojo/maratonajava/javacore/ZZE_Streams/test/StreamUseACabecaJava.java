a9a023827d339fbdaf554f5e97427f59334094ad
DANR1BEIRO <danribeirodev@gmail.com>
2025-12-28 12:23:06 -0300

    test: Streams

---
+++ src/academy/devdojo/maratonajava/javacore/ZZE_Streams/test/StreamUseACabecaJava.java
@@ -0,0 +1,37 @@
+package academy.devdojo.maratonajava.javacore.ZZE_Streams.test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamUseACabecaJava {
+    static void main() {
+        List<String> strings = List.of("I", "am", "a", "list", "of", "Strings");
+        System.out.println(strings);
+
+        System.out.println("------------------------");
+
+        Stream<String> stream = strings.stream();
+        Stream<String> limit = stream.limit(4);
+        List<String> result = limit.collect(Collectors.toList());
+        System.out.println("result: " + result);
+
+        System.out.println("------------------------");
+
+        List<String> collect = strings.stream()
+                .limit(4)
+                .toList();
+        System.out.println("result: " + collect);
+
+        System.out.println("------------------------");
+
+        List<String> collect1 = strings.stream()
+                .sorted((s, t1) -> s.compareToIgnoreCase(t1))
+                .skip(2)
+                .limit(4)
+                .toList();
+        System.out.println("result: " + collect1); // Ordem natural coloca letra maiúscula antes de minúscula
+
+
+    }
+}
