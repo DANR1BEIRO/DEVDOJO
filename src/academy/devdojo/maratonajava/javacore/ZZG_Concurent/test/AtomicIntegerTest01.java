61f8fae025e11e308ae48fb0ced329f6c4fc5c44
DANR1BEIRO <danribeirodev@gmail.com>
2026-01-12 12:21:39 -0300

    Aula 229: Concurrency pt 01 - AtomicInteger

---
+++ src/academy/devdojo/maratonajava/javacore/ZZG_Concurent/test/AtomicIntegerTest01.java
@@ -0,0 +1,51 @@
+package academy.devdojo.maratonajava.javacore.ZZG_Concurent.test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * AtomicInteger
+ * uses a Compare-And-Swap (CAS) operation internally to perform thread-safe
+ * updates to a single integer value without using explicit locks. The core
+ * method is compareAndSet(), which is a hardware-level atomic instruction that
+ * ensures the check and act operations happen in a single, uninterruptible step
+ */
+
+class Counter {
+    private int count;
+    AtomicInteger atomicInteger = new AtomicInteger();
+
+    void increment() {
+        count++;
+        atomicInteger.incrementAndGet();
+    }
+
+    int getCount() {
+        return count;
+    }
+
+    public AtomicInteger getAtomicInteger() {
+        return atomicInteger;
+    }
+}
+
+public class AtomicIntegerTest01 {
+    static void main() throws InterruptedException {
+        Counter counter = new Counter();
+        Runnable runnable = () -> {
+            for (int i = 0; i < 10000; i++) {
+                counter.increment();
+            }
+        };
+
+        Thread t1 = new Thread(runnable);
+        Thread t2 = new Thread(runnable);
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        System.out.println(counter.getCount());
+        System.out.println(counter.getAtomicInteger());
+    }
+}
