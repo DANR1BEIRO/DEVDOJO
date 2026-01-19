e447e4f7cedc1a8d9cf92e5dae34d7ae06e038f6
DANR1BEIRO <danribeirodev@gmail.com>
2026-01-19 12:21:39 -0300

    feat: Adiciona prints para facilitar o entendimento do que acontece com o iterator enquanto ele está 'dormindo'por 2seg.

--- src/academy/devdojo/maratonajava/javacore/ZZG_Concurrent/test/CopyOnWriteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZG_Concurrent/test/CopyOnWriteTest01.java
@@ -8,14 +8,16 @@ import java.util.concurrent.TimeUnit;
 public class CopyOnWriteTest01 {
     static void main() {
         List<Integer> list = new CopyOnWriteArrayList<>();
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 10; i++) {
             list.add(i);
         }
 
         Runnable runnableIterator = () -> {
             Iterator<Integer> iterator = list.iterator();
+            System.out.println("Thread Iterator: Snapshot taken:");
             try {
                 TimeUnit.SECONDS.sleep(2);
+                System.out.println("Thread Iterator printing snapshot:");
                 iterator.forEachRemaining(System.out::println);
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -23,12 +25,18 @@ public class CopyOnWriteTest01 {
         };
 
         Runnable runnableRemover = () -> {
-            for (int i = 0; i < 5; i++) {
-                System.out.printf("%s removed %d%n", Thread.currentThread().getName(), i);
+            try {
+                TimeUnit.SECONDS.sleep(1); // wait 1 second so the iterator starts first
+                for (int i = 0; i < 5; i++) {
+                    if (list.remove(Integer.valueOf(i))) {
+                        System.out.printf("Thread Remover: %d removed. Current list size: %d%n", i, list.size());
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         };
 
-        new Thread(runnableIterator).start();
         new Thread(runnableIterator).start();
         new Thread(runnableRemover).start();
     }
