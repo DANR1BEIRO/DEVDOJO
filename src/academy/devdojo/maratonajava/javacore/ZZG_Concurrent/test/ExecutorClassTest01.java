b44dd3def9b0b849f6eac7cf34d712afef722ada
DANR1BEIRO <danribeirodev@gmail.com>
2026-01-22 12:21:16 -0300

    Aula 236: Concurrency pt 08 - Executors pt 01 - Thread Pool

---
+++ src/academy/devdojo/maratonajava/javacore/ZZG_Concurrent/test/ExecutorClassTest01.java
@@ -0,0 +1,47 @@
+package academy.devdojo.maratonajava.javacore.ZZG_Concurrent.test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class Printer implements Runnable {
+    private final int num;
+
+    public Printer(int num) {
+        this.num = num;
+    }
+
+    @Override
+    public void run() {
+        System.out.printf("%s start: %d%n", Thread.currentThread().getName(), num);
+
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("%s Finished%n", Thread.currentThread().getName());
+    }
+}
+
+public class ExecutorClassTest01 {
+    static void main() {
+        //System.out.println(Runtime.getRuntime().availableProcessors());
+
+        // Executor: Implemented to decouple task submission from execution
+        // ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        // ExecutorService executorService = Executors.newSingleThreadExecutor();
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(new Printer(1));
+        executorService.execute(new Printer(2));
+        executorService.execute(new Printer(3));
+        executorService.execute(new Printer(4));
+        executorService.execute(new Printer(5));
+        executorService.execute(new Printer(6));
+
+        executorService.shutdown();
+        System.out.println("application finished");
+
+
+    }
+}
