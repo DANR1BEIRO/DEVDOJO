1c2c9c3b0bda7b2026b62f40dd84b216609bbba3
DANR1BEIRO <danribeirodev@gmail.com>
2026-01-24 12:17:20 -0300

    Aula 239: Concurrency pt 11 - Executors pt 04 - Future

---
+++ src/academy/devdojo/maratonajava/javacore/ZZG_Concurrent/test/FutureTest01.java
@@ -0,0 +1,35 @@
+package academy.devdojo.maratonajava.javacore.ZZG_Concurrent.test;
+
+import java.util.concurrent.*;
+
+public class FutureTest01 {
+    static void main() {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<Double> dollarRequest = executorService.submit(() -> {
+            TimeUnit.SECONDS.sleep(5);
+            return 5.30D;
+        });
+
+        System.out.println(doSomething());
+        try {
+
+            // O valor recebido no parâmetro define quanto tempo a Thread esperará
+            // pelo dado até que lance um TimeoutException.
+            System.out.println(dollarRequest.get(3, TimeUnit.SECONDS));
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            e.printStackTrace();
+        } finally {
+            executorService.shutdown();
+        }
+    }
+
+    private static long doSomething() {
+        System.out.println(Thread.currentThread().getName());
+        long sum = 0;
+        for (int i = 0; i < 1_000_000; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
+wq
