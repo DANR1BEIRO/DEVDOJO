8d4600d94d7b7e5ff08911008410a15e3849c850
DANR1BEIRO <danribeirodev@gmail.com>
2026-01-25 17:33:25 -0300

    test: Test Effective Java book's Pizza Builder example

---
+++ src/academy/devdojo/maratonajava/javacore/ZZH_designpatterns/domain/Calzone.java
@@ -0,0 +1,37 @@
+package academy.devdojo.maratonajava.javacore.ZZH_designpatterns.domain;
+
+public class Calzone extends Pizza {
+    private final boolean sauceInside;
+
+    public static class Builder extends Pizza.Builder<Builder> {
+        private boolean sauceInside = false; // default
+
+        public Builder sauceInside() {
+            sauceInside = true;
+            return this;
+        }
+
+        @Override
+        public Calzone build() {
+            return new Calzone(this);
+        }
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Calzone{" +
+                "sauceInside=" + sauceInside +
+                ", toppings=" + toppings +
+                '}';
+    }
+
+    private Calzone(Builder builder) {
+        super(builder);
+        sauceInside = builder.sauceInside;
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/ZZH_designpatterns/domain/NyPizza.java
@@ -0,0 +1,40 @@
+package academy.devdojo.maratonajava.javacore.ZZH_designpatterns.domain;
+
+import java.util.Objects;
+
+public class NyPizza extends Pizza {
+    public enum Size {SMALL, MEDIUM, LARGE}
+
+    private final Size size;
+
+    public static class Builder extends Pizza.Builder<Builder> {
+        private final Size size;
+
+        public Builder(Size size) {
+            this.size = Objects.requireNonNull(size);
+        }
+
+        @Override
+        public NyPizza build() {
+            return new NyPizza(this);
+        }
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "NyPizza{" +
+                "size=" + size +
+                ", toppings=" + toppings +
+                '}';
+    }
+
+    private NyPizza(Builder builder) {
+        super(builder);
+        size = builder.size;
+    }
+}
