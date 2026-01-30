74ddd66f1c03744f83074b55bc414f69a20faf2b
DANR1BEIRO <danribeirodev@gmail.com>
2026-01-30 10:41:56 -0300

    Aula 260: JDBC pt 09 - Atualizando dados com Statement

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZI_JDBC/test/ConnectionFactoryTest01.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZI_JDBC/test/ConnectionFactoryTest01.java
@@ -1,14 +1,17 @@
 package academy.devdojo.maratonajava.javacore.ZZI_JDBC.test;
 
 import academy.devdojo.maratonajava.javacore.ZZI_JDBC.domain.Producer;
+import academy.devdojo.maratonajava.javacore.ZZI_JDBC.service.ProducerService;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
         Producer producer = Producer.builder().name("Studio Deen").build();
+        Producer producerToUpdate = Producer.builder().id(1).name("MADHOUSE").build();
         //ProducerService.save(producer);
         //ProducerService.delete(12);
+        ProducerService.update(producerToUpdate);
 
 
     }
