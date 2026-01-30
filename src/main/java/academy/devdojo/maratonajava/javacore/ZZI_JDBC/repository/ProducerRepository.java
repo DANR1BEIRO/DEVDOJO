ec2e79585632a00d3b250166c35b88992a4a26b3
DANR1BEIRO <danribeirodev@gmail.com>
2026-01-30 11:03:25 -0300

    Aula 261: JDBC pt 10 - Buscando dados com ResultSet pt 01 - findAll

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZI_JDBC/repository/ProducerRepository.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZI_JDBC/repository/ProducerRepository.java
@@ -5,8 +5,11 @@ import academy.devdojo.maratonajava.javacore.ZZI_JDBC.domain.Producer;
 import lombok.extern.log4j.Log4j2;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 @Log4j2 // log library
 public class ProducerRepository {
@@ -43,10 +46,30 @@ public class ProducerRepository {
              Statement stmt = conn.createStatement()) {
             int rowsAffected = stmt.executeUpdate(sql);
             log.info("Updated producer '{}'. Database rows affected '{}'", producer.getId(), rowsAffected);
-
-
         } catch (SQLException e) {
             log.error("Error while trying to update producer '{}'", producer.getId(), e);
         }
+    }
+
+    public static List<Producer> findAll() {
+        log.info("Finding all producers");
+        String sql = "SELECT id, name FROM anime_store.producer;";
+        List<Producer> producers = new ArrayList<>();
+
+        try (Connection conn = ConnectionFactory.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                Producer producer = Producer.builder()
+                        .id(rs.getInt("id"))
+                        .name(rs.getString("name"))
+                        .build();
+                producers.add(producer);
+            }
+
+        } catch (SQLException e) {
+            log.error("Error while trying to find all producers", e);
+        }
+        return producers;
     }
 }
--- src/main/java/academy/devdojo/maratonajava/javacore/ZZI_JDBC/test/ConnectionFactoryTest01.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZI_JDBC/test/ConnectionFactoryTest01.java
@@ -4,15 +4,19 @@ import academy.devdojo.maratonajava.javacore.ZZI_JDBC.domain.Producer;
 import academy.devdojo.maratonajava.javacore.ZZI_JDBC.service.ProducerService;
 import lombok.extern.log4j.Log4j2;
 
+import java.util.List;
+
 @Log4j2
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
         Producer producer = Producer.builder().name("Studio Deen").build();
         Producer producerToUpdate = Producer.builder().id(1).name("MADHOUSE").build();
+
         //ProducerService.save(producer);
         //ProducerService.delete(12);
-        ProducerService.update(producerToUpdate);
+        //ProducerService.update(producerToUpdate);
 
-
+        List<Producer> producerList = ProducerService.findAll();
+        log.info("Producers found '{}'", producerList);
     }
 }
