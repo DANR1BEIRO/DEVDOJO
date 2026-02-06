706e03aa301768aa3230dcb5bd372e96203d7dbe
DANR1BEIRO <danribeirodev@gmail.com>
2026-02-06 12:31:53 -0300

    Aula 275: JDBC pt 24 - CRUD pt 01 - findByName and findAll

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZJ_CRUD/domain/Producer.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.ZZJ_CRUD.domain;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+public class Producer {
+    Integer id;
+    String name;
+}
+
