f197c3a406aded0ae0ba9710b5f390684f087c29
DANR1BEIRO <danribeirodev@gmail.com>
2026-02-07 11:08:33 -0300

    Aula 277: JDBC pt 26 - CRUD pt 03 - save

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZJ_CRUD/service/ProducerService.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZJ_CRUD/service/ProducerService.java
@@ -3,7 +3,6 @@ package academy.devdojo.maratonajava.javacore.ZZJ_CRUD.service;
 import academy.devdojo.maratonajava.javacore.ZZJ_CRUD.domain.Producer;
 import academy.devdojo.maratonajava.javacore.ZZJ_CRUD.repository.ProducerRepository;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class ProducerService {
@@ -11,34 +10,34 @@ public class ProducerService {
 
     public static void menu(int op) {
         switch (op) {
-            case 1:
-                findByName();
-                break;
-            case 2:
-                delete();
-                break;
-            default:
-                throw new IllegalArgumentException("Not a valid option");
+            case 1 -> findByName();
+            case 2 -> delete();
+            case 3 -> save();
+            default -> throw new IllegalArgumentException("Not a valid option");
         }
     }
 
     private static void findByName() {
         System.out.println("Type the name or empty to all");
         String name = SCANNER.nextLine();
-        List<Producer> producers = ProducerRepository.findByname(name);
-        for (int i = 0; i < producers.size(); i++) {
-            Producer producer = producers.get(i);
-            System.out.printf("[%d] - %d | %s%n", i, producer.getId(), producer.getName());
-        }
+        ProducerRepository.findByname(name)
+                .forEach(p -> System.out.printf("%d | %s%n", p.getId(), p.getName()));
     }
 
     private static void delete() {
         System.out.println("Type the ID of the producer you want to delete");
         int id = Integer.parseInt(SCANNER.nextLine());
-        System.out.println("Are you sure? S/N");
+        System.out.println("Are you sure? Y/N");
         String choice = SCANNER.nextLine();
         if ("s".equalsIgnoreCase(choice)) {
             ProducerRepository.delete(id);
         }
+    }
+
+    private static void save() {
+        System.out.println("Type the name of the producer");
+        String name = SCANNER.nextLine();
+        Producer producer = Producer.builder().name(name).build();
+        ProducerRepository.save(producer);
     }
 }
