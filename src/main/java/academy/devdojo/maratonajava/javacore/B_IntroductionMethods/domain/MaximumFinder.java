2c6d799e0988697cd75b9138db71632c6b51d310
DANR1BEIRO <danribeirodev@gmail.com>
2026-02-09 10:02:41 -0300

    refactor: Utiliza o static method max da classe Math para determinar o maior valor

--- src/main/java/academy/devdojo/maratonajava/javacore/B_IntroductionMethods/domain/MaximumFinder.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/B_IntroductionMethods/domain/MaximumFinder.java
@@ -8,15 +8,11 @@ public class MaximumFinder {
     }
 
     public static Double maximum(Double x, Double y, Double z) {
-        Double maximumValue = x;
 
         Objects.requireNonNull(x, "Parameter cannot be null");
         Objects.requireNonNull(y, "Parameter cannot be null");
         Objects.requireNonNull(z, "Parameter cannot be null");
 
-        if (y > maximumValue) maximumValue = y;
-        if (z > maximumValue) maximumValue = z;
-
-        return maximumValue;
+        return Math.max(x, Math.max(y, z));
     }
 }
