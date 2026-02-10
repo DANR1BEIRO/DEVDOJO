7107da46096573392efa7208fd9e350d4aa15955
DANR1BEIRO <danribeirodev@gmail.com>
2026-02-10 10:54:20 -0300

    feat: Implementa uma classe de teste para o Jokenpo, chamando o metodo go() de forma estatica

--- src/main/java/academy/devdojo/maratonajava/javacore/B_IntroductionMethods/test/TestJoKenPo01.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/B_IntroductionMethods/test/TestJoKenPo01.java
@@ -2,7 +2,7 @@ package academy.devdojo.maratonajava.javacore.B_IntroductionMethods.test;
 
 import academy.devdojo.maratonajava.javacore.B_IntroductionMethods.domain.JoKenPo;
 
-public class TestJoKenPo {
+public class TestJoKenPo01 {
     public static void main(String[] args) {
         JoKenPo.go();
     }
